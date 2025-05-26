33d71690e174b316c0ef96504c6e338801557644
dvt-dev <dvtuan.dev@gmail.com>
2025-05-26 23:41:12 +0700

    feat: ui chat and refactor code

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import {FaPhone, FaVideo, FaCircleInfo } from "react-icons/fa6";
+import { MdAttachFile } from "react-icons/md";
+import { IoSend } from "react-icons/io5";
+
 import classNames from 'classnames/bind';
 import styles from './Chat.module.scss';
 
@@ -7,7 +11,50 @@ const cx = classNames.bind(styles);
 
 const Chat = () => {
   return (
-    <div className={cx('chat-container')}>Chat</div>
+    <div className={cx('chat-container')}>
+      <div className={cx('chat__header')}>
+        <div className={cx('chat__header-info')}>
+          <div className={cx('chat__header-avatar')}>
+            <img src='https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg' alt="" className={cx('chat__header-avatar-img')}></img>
+          </div>
+          <div className={cx('chat__header-name')}>
+            <h4 className={cx('chat__header-name-text')}>Đỗ Văn Tuấn</h4>
+          </div>
+        </div>
+        <div className={cx('chat__header-icons')}>
+            <div className={cx('chat__header-icon')}>
+              <FaPhone  size={20}/>
+            </div>
+            <div className={cx('chat__header-icon')}>
+              <FaVideo size={20}  />
+            </div>
+            <div className={cx('chat__header-icon')}>
+              <FaCircleInfo size={20} />
+            </div>
+          </div>
+      </div>
+      <div className={cx('messages-list')}>
+
+      </div>
+      <div className={cx('message-form')}>
+        <div className={cx('message-form-container')}>
+          <div className={cx('message-form__media-file')}>
+            <input type="file" className={cx('message-form__media-file-input')}/>
+            <button className={cx('message-form__media-file-btn')}>
+              <MdAttachFile size={25} />
+            </button>
+          </div>
+          <div className={cx('message-form__input')}>
+            <input type="text" placeholder='Nhập tin nhắn...' className={cx('message-form__input-field')}/>
+          </div>
+        </div>       
+        <div className={cx('message-form__send')}>
+          <button className={cx('message-form__send-btn')}>
+            <IoSend size={30}/>
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { GoSearch } from "react-icons/go";
 
 import classNames from 'classnames/bind';
 import styles from './ChatList.module.scss';
@@ -6,7 +7,93 @@ const cx = classNames.bind(styles);
 
 const ChatList = () => {
   return (
-    <div className={cx('chat-list-container')}>ChatList</div>
+    <div className={cx('chat-list-container')}>
+      <div className={cx('search')}>
+        <div className={cx('search-bar')}>
+          <div className={cx('search-icon')}>
+            <GoSearch size={24} />
+          </div>
+          <input type="text" placeholder="Tìm kiếm..." className={cx('search-input')} />
+        </div>
+      </div>
+      <div className={cx('chat-list')}>
+        <div className={cx('chat-item')}>
+          <div className={cx('chat-item-avatar')}>
+            <img src="https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg" alt="user" />
+          </div>
+          <div className={cx('chat-item-info')}>
+            <h2 className={cx('chat-item-info__name')}>Đỗ Văn Tuấn</h2>
+            <p className={cx('chat-item-info__message')}>Hello</p>
+          </div>
+        </div>
+        <div className={cx('chat-item')}>
+          <div className={cx('chat-item-avatar')}>
+            <img src="https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg" alt="user" />
+          </div>
+          <div className={cx('chat-item-info')}>
+            <h2 className={cx('chat-item-info__name')}>Đỗ Văn Tuấn</h2>
+            <p className={cx('chat-item-info__message')}>Hello</p>
+          </div>
+        </div>
+        <div className={cx('chat-item')}>
+          <div className={cx('chat-item-avatar')}>
+            <img src="https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg" alt="user" />
+          </div>
+          <div className={cx('chat-item-info')}>
+            <h2 className={cx('chat-item-info__name')}>Đỗ Văn Tuấn</h2>
+            <p className={cx('chat-item-info__message')}>Hello</p>
+          </div>
+        </div>
+        <div className={cx('chat-item')}>
+          <div className={cx('chat-item-avatar')}>
+            <img src="https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg" alt="user" />
+          </div>
+          <div className={cx('chat-item-info')}>
+            <h2 className={cx('chat-item-info__name')}>Đỗ Văn Tuấn</h2>
+            <p className={cx('chat-item-info__message')}>Hello</p>
+          </div>
+        </div>
+        <div className={cx('chat-item')}>
+          <div className={cx('chat-item-avatar')}>
+            <img src="https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg" alt="user" />
+          </div>
+          <div className={cx('chat-item-info')}>
+            <h2 className={cx('chat-item-info__name')}>Đỗ Văn Tuấn</h2>
+            <p className={cx('chat-item-info__message')}>Hello</p>
+          </div>
+        </div>
+        <div className={cx('chat-item')}>
+          <div className={cx('chat-item-avatar')}>
+            <img src="https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg" alt="user" />
+          </div>
+          <div className={cx('chat-item-info')}>
+            <h2 className={cx('chat-item-info__name')}>Đỗ Văn Tuấn</h2>
+            <p className={cx('chat-item-info__message')}>Hello</p>
+          </div>
+        </div>
+        <div className={cx('chat-item')}>
+          <div className={cx('chat-item-avatar')}>
+            <img src="https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg" alt="user" />
+          </div>
+          <div className={cx('chat-item-info')}>
+            <h2 className={cx('chat-item-info__name')}>Đỗ Văn Tuấn</h2>
+            <p className={cx('chat-item-info__message')}>Hello</p>
+          </div>
+        </div>
+        <div className={cx('chat-item')}>
+          <div className={cx('chat-item-avatar')}>
+            <img src="https://www.svgrepo.com/show/382106/male-avatar-boy-face-man-user-9.svg" alt="user" />
+          </div>
+          <div className={cx('chat-item-info')}>
+            <h2 className={cx('chat-item-info__name')}>Đỗ Văn Tuấn</h2>
+            <p className={cx('chat-item-info__message')}>Hello</p>
+          </div>
+        </div>
+        
+        
+        
+      </div>
+    </div>
   )
 }
 
